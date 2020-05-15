a8b8c2d568ec2c41c79a815a93304d708e94d4f1
Fábio <fabiopv1994@gmail.com>
2020-05-15 12:44:03 -0300

    correção de bug no helper

--- src/Helpers/from_to_data.php
+++ src/Helpers/from_to_data.php
@@ -3,6 +3,6 @@
 if (!function_exists('from_to_data')) {
     function from_to_data($param = null)
     {
-        return config('from_to_data')[get_class($param)];
+        return config('from_to_data')[get_class($param)] ?? null;
     }
 }
