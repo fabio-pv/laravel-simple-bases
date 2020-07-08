f167d424100343e90dd3df3aada241736e6bd6cc
Fábio <fabiopv1994@gmail.com>
2020-07-08 11:06:57 -0300

    testando command do laravel

---
+++ src/Command/KrloveEloquentModelGenerator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LaravelSimpleBases\Commands;
+
+use Illuminate\Console\Command;
+
+class KrloveEloquentModelGenerator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fabio-pv:krlove-eloquent-model-generator-config';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        dd('oioioioioi');
+        return 0;
+    }
+}
