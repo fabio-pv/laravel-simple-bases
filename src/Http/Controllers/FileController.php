5a834305a077723f696f1056737b6d86f2e4f63f
Fábio <fabiopv1994@gmail.com>
2020-08-12 11:28:13 -0300

    add documentação para FileController

--- src/Http/Controllers/FileController.php
+++ src/Http/Controllers/FileController.php
@@ -8,9 +8,24 @@ use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use LaravelSimpleBases\Utils\StatusCodeUtil;
 
+/**
+ * @group File
+ * @package LaravelSimpleBases\Http\Controllers
+ */
 class FileController
 {
 
+    /**
+     * File
+     *
+     * @urlParam path required
+     * @urlParam uuid required
+     *
+     * @param $path
+     * @param $photo
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
     public function retrive($path, $photo)
     {
         try {
